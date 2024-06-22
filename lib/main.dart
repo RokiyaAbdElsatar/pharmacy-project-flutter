b4c31dd3bfaf9aca9e6913b8fadd1ab9d2cec340
Rokiya2932004 <rokiyaabdelsatar293@gmail.com>
2024-06-22 14:51:01 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:pharmacy_project/screans/startpage.dart';
+
+import 'auth/login.dart';
+import 'auth/signin.dart';
+
+void main() {
+  runApp( Pharmacy());
+}
+
+class Pharmacy extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: StartPage ()
+    );
+
+  }
+
+  // startpage() {}
+}
+
+/*
+Login(),
+        routes: {
+          "signup" : (context) => SignUp() ,
+          "login" : (context) => Login()
+        }
+
+
+*/
